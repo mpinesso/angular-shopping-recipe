abbb09eb7c0fcdc7db0633ae6f252162234dafd3
Marco Pinesso <pinesso.marco@gmail.com>
2017-08-30 15:15:59 +0200

    Added shopping component

---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,5 @@
+export class Ingredient{
+
+  // con questa dicitura vengono direttamente generati gli attributi "name" e amount per la classe
+  constructor(public name: string, public amount: number){}
+}
