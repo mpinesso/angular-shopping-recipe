fd2fb49e804d99f22101101dca311573d5e82924
Marco Pinesso <pinesso.marco@gmail.com>
2017-09-04 14:24:29 +0200

    Added directives on menus

---
+++ src/app/shared/dropdown.directive.ts
@@ -0,0 +1,22 @@
+import { Directive,
+  Input,
+  TemplateRef,
+  ViewContainerRef,
+  ElementRef,
+  Renderer2,
+  HostListener,
+  HostBinding} from '@angular/core';
+
+@Directive({
+  selector: '[appDropdown]'
+})
+export class DropdownDirective {
+  @HostBinding('class.open') toggleMenu: boolean = false;
+
+  @HostListener('click') onToggleMenu(eventData: Event) {
+    this.toggleMenu = !this.toggleMenu;
+  }
+  @HostListener('mouseleave') mouseleave(eventData: Event) {
+    this.toggleMenu = false;
+  }
+}
